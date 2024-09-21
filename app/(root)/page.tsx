6f64f0dc6ede61e36abce5ddacd2dd977ad461de
RunorTech <ighorodjeoghenerunor@gmail.com>
2024-09-21 06:11:11 -0700

    last

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,20 +6,38 @@ import { useEffect, useState } from "react";
 
 
 export default function Home() {
+  const [getUserInfo, setGetUserInfo] = useState<any>();
+  const [registeredUser, setRegisteredUser] = useState<boolean>(false);
+  const [isLoggedIn, setisLoggedIn] = useState<boolean>(false);
 
   useEffect(() => {
     initializeServer();
   }, [])
   
   const initializeServer = async () => {
-    const response = axios.get('http://localhost:8000');
-    console.log(response);
+   try {
+    const response = await axios.get('http://localhost:8000');
+    setGetUserInfo(response.data);
+   } catch (error) {
+    console.log(error)
+   }
+  
   }
-  const [registeredUser, setRegisteredUser] = useState<boolean>(true);
-  const [isLoggedIn, setisLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    checkreg();
+  },[getUserInfo])
+
+  const checkreg = () => {
+    if(getUserInfo == "guest"){
+      setRegisteredUser(false);
+    }else{
+      setRegisteredUser(true);
+    }
+  }
 
   return (
-    <>  <h1 className="">my home page</h1>
+    <>  <h1 className="">Welcome to home page {getUserInfo}</h1>
       {isLoggedIn ? (<a href="/sign-in">SignOut</a>) : registeredUser ? <a href="/sign-in">signIn</a> : <a href="/sign-up">Register</a>}
 
     </>
